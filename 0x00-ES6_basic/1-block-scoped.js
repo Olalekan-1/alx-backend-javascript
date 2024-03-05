c38419c47b3048466655b830041375d3b01e8871
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-05 01:41:26 +0100

    updates solutions

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+export default function taskBlock (trueOrFalse) {
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-   task = task;
-   task2 = task2;
+    task = task;
+    task2 = task2;
   }
 
   return [task, task2];
 }
-
