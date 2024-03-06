8caf2c3e84721ac89ecbc81d575085befad16bed
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-06 23:13:42 +0100

    add solution

--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,4 +1,8 @@
 import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+const test = handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
+console.log(test);
+
+// const random = console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+
 
