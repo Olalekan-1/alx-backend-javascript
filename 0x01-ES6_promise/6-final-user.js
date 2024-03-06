eafcbe672f86e87d82e7aff63f35585ea475ef28
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-06 18:17:14 +0100

    add solution

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })))
+    .catch((error) => {
+      console.error('Error occurred during profile signup:', error);
+      return [];
+    });
+}
