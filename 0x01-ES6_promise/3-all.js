91deac245fd6800f907df613508b21cfef9a09b3
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-06 17:44:40 +0100

    add solution

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch((error) => {
-      console.error('Signup system offline:', error);
+    .catch(() => {
+      console.log('Signup system offline:');
     });
 }
