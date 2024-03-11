dafca6c9b1f1aff423f766fd88be8ab153605648
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-11 18:38:30 +0100

    add soltion 0 and set up

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents() {
+    let obj1 = { id: 1, firstName: "Guillaume", location: "San Francisco"};
+    let obj2 = { id: 2, firstName: "James", location: "Columbia"};
+    let obj3 = { id: 3, firstName: "'Serena", location: "San Francisco"};
+
+    const arr = [obj1, obj2, obj3];
+
+    return arr;
+
+}
