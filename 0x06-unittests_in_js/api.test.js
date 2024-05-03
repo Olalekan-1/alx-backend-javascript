5b8314520b69edd976f9d7e05f334d11f6d150fb
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-05-03 01:38:25 +0100

    add solution

---
+++ 0x06-unittests_in_js/api.test.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const expect = require('chai').expect;
+const server = require('./api');
+
+describe('Index Page', function() {
+  after(function() {
+    server.close();
+  });
+
+  it('should return status code 200', function(done) {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct result', function(done) {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
