736e325cf35c157c116405974022d4fd5d12ccf0
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-05-03 02:29:59 +0100

    add solution

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,31 +1,31 @@
 const { expect } = require('chai');
 
-describe('Testing numbers', () => {
-  it('1 is equal to 1', () => {
+describe.('Testing numbers', () => {
+  it.skip('1 is equal to 1', () => {
     expect(1 === 1).to.be.true;
   });
 
-  it('2 is equal to 2', () => {
+  it.skip('2 is equal to 2', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
+  it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
 
-  it('3 is equal to 3', () => {
+  it.skip('3 is equal to 3', () => {
     expect(3 === 3).to.be.true;
   });
 
-  it('4 is equal to 4', () => {
+  it.skip('4 is equal to 4', () => {
     expect(4 === 4).to.be.true;
   });
 
-  it('5 is equal to 5', () => {
+  it.skip('5 is equal to 5', () => {
     expect(5 === 5).to.be.true;
   });
 
-  it('6 is equal to 6', () => {
+  it.skip('6 is equal to 6', () => {
     expect(6 === 6).to.be.true;
   });
 
