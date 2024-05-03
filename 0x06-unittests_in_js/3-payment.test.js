ab697ca1c68a9d6733997271948714ec02880978
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-05-03 01:25:39 +0100

    add solution

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with the correct arguments', function() {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
+
